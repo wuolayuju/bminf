09de4432a8a01756f275070a7041ceb7deb75260
pinwi <pinwi@debian>
2014-02-12 21:38:47 +0100

    comentado TextParser y HTMLSimpleParser

--- Practica2/src/es/uam/eps/bmi/search/parsing/TextParser.java
+++ Practica2/src/es/uam/eps/bmi/search/parsing/TextParser.java
@@ -1,16 +1,21 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package es.uam.eps.bmi.search.parsing;
 
 import java.io.IOException;
 
 /**
- *
- * @author e185318
+ * Interfaz para procesar un texto
+ * @author Ari Handler - Adrián Lorenzo
  */
 public interface TextParser {
     
+    /**
+     * 
+     * Procesa un texto dado
+     * 
+     * @param text
+     * @return texto procesado
+     * @throws IOException 
+     */
     public String parse(String text) throws IOException;
 }
