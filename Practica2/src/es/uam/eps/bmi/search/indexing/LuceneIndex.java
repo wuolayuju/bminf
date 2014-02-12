f71fbb8cccb787c6f48cef7228d5fc79cf098b1f
UAM <UAM@PC-SOAYAAE>
2014-02-12 11:17:31 +0100

    Comenzado de lectura de de ficheros comprmidos de los documentos en vez de directorio

--- Practica2/src/es/uam/eps/bmi/search/indexing/LuceneIndex.java
+++ Practica2/src/es/uam/eps/bmi/search/indexing/LuceneIndex.java
@@ -14,9 +14,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -202,84 +205,77 @@ public class LuceneIndex implements Index{
     }
     
     private void indexDocs(IndexWriter writer, File file, TextParser parser) throws IOException{
-     // do not try to index files that cannot be read
-    if (file.canRead()) {
-      if (file.isDirectory()) {
-        String[] files = file.list();
-        // an IO error could occur
-        if (files != null) {
-          for (int i = 0; i < files.length; i++) {
-            indexDocs(writer, new File(file, files[i]), parser);
-          }
+        // do not try to index files that cannot be read
+        if (file.canRead()) {
+
+            ZipInputStream zis;
+            try {
+              zis = new ZipInputStream(new FileInputStream(file));
+            } catch (FileNotFoundException fnfe) {
+              // at least on windows, some temporary files raise this exception with an "access denied" message
+              // checking if the file can be read doesn't help
+              return;
+            }
+
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry())!=null)
+            {
+                try {
+
+                    String filePath = file.getPath()+"\\"+entry.getName();
+                    
+                    // make a new, empty document
+                    Document doc = new Document();
+
+                    // Add the path of the file as a field named "path".  Use a
+                    // field that is indexed (i.e. searchable), but don't tokenize 
+                    // the field into separate words and don't index term frequency
+                    // or positional information:
+                    Field pathField = new Field("path", filePath, Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
+                    pathField.setIndexOptions(FieldInfo.IndexOptions.DOCS_ONLY);
+                    doc.add(pathField);
+
+                    // Add the last modified date of the file a field named "modified".
+                    // Use a NumericField that is indexed (i.e. efficiently filterable with
+                    // NumericRangeFilter).  This indexes to milli-second resolution, which
+                    // is often too fine.  You could instead create a number based on
+                    // year/month/day/hour/minutes/seconds, down the resolution you require.
+                    // For example the long value 2011021714 would mean
+                    // February 17, 2011, 2-3 PM.
+                    NumericField modifiedField = new NumericField("modified");
+                    modifiedField.setLongValue(file.lastModified());
+                    doc.add(modifiedField);
+
+                    // Add the contents of the file to a field named "contents".  Specify a Reader,
+                    // so that the text of the file is tokenized and indexed, but not stored.
+                    // Note that FileReader expects the file to be in UTF-8 encoding.
+                    // If that's not the case searching for special characters will fail.
+                    int len = 0;
+                    byte[] buffer = new byte[2048];
+                    String text = "";
+                    while ((len = zis.read(buffer, 0, len)) > 0) {
+                        text += Arrays.toString(buffer);
+                    }
+                    doc.add(new Field("contents",parser.parse(text), Field.Store.YES, Field.Index.ANALYZED));
+
+                    if (writer.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE) {
+                      // New index, so we just add the document (no old document can be there):
+                      System.out.println("adding " + filePath);
+                      writer.addDocument(doc);
+                    } else {
+                      // Existing index (an old copy of this document may have been indexed) so 
+                      // we use updateDocument instead to replace the old one matching the exact 
+                      // path, if present:
+                      System.out.println("updating " + filePath);
+                      writer.updateDocument(new Term("path", filePath), doc);
+                    }
+
+                } finally {
+                  zis.close();
+                }
+            }
         }
-      } else {
-
-        FileInputStream fis;
-        try {
-          fis = new FileInputStream(file);
-        } catch (FileNotFoundException fnfe) {
-          // at least on windows, some temporary files raise this exception with an "access denied" message
-          // checking if the file can be read doesn't help
-          return;
-        }
-
-        try {
-
-          // make a new, empty document
-          Document doc = new Document();
-
-          // Add the path of the file as a field named "path".  Use a
-          // field that is indexed (i.e. searchable), but don't tokenize 
-          // the field into separate words and don't index term frequency
-          // or positional information:
-          Field pathField = new Field("path", file.getPath(), Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
-          pathField.setIndexOptions(FieldInfo.IndexOptions.DOCS_ONLY);
-          doc.add(pathField);
-
-          // Add the last modified date of the file a field named "modified".
-          // Use a NumericField that is indexed (i.e. efficiently filterable with
-          // NumericRangeFilter).  This indexes to milli-second resolution, which
-          // is often too fine.  You could instead create a number based on
-          // year/month/day/hour/minutes/seconds, down the resolution you require.
-          // For example the long value 2011021714 would mean
-          // February 17, 2011, 2-3 PM.
-          NumericField modifiedField = new NumericField("modified");
-          modifiedField.setLongValue(file.lastModified());
-          doc.add(modifiedField);
-
-          // Add the contents of the file to a field named "contents".  Specify a Reader,
-          // so that the text of the file is tokenized and indexed, but not stored.
-          // Note that FileReader expects the file to be in UTF-8 encoding.
-          // If that's not the case searching for special characters will fail.
-          BufferedReader read = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
-           String text = "", line;
-            
-            do {
-                line = read.readLine();
-                text += line;
-            } while (line != null);
-            
-            
-          doc.add(new Field("contents",parser.parse(text), Field.Store.YES, Field.Index.ANALYZED));
-
-          if (writer.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE) {
-            // New index, so we just add the document (no old document can be there):
-            System.out.println("adding " + file);
-            writer.addDocument(doc);
-          } else {
-            // Existing index (an old copy of this document may have been indexed) so 
-            // we use updateDocument instead to replace the old one matching the exact 
-            // path, if present:
-            System.out.println("updating " + file);
-            writer.updateDocument(new Term("path", file.getPath()), doc);
-          }
-          
-        } finally {
-          fis.close();
-        }
-      }
-    }
-  }       
+    }      
     
     
     public static void  main(String[] args) {
@@ -305,7 +301,7 @@ public class LuceneIndex implements Index{
                     break;
             }
         }
-        //index.build(docsPath,indexPath,p);
+        index.build(docsPath,indexPath,p);
         
         index.load(indexPath);
         
