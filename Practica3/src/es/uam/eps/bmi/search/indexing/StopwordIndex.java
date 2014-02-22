15c8cbe2c703130c8ba0729bb5d345bbcf75952b
Ari <ari.hangam@gmail.com>
2014-02-22 10:57:02 +0100

    Añadidos los esqueletos de los indexadores.

---
+++ Practica3/src/es/uam/eps/bmi/search/indexing/StopwordIndex.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.uam.eps.bmi.search.indexing;
+
+import es.uam.eps.bmi.search.TextDocument;
+import es.uam.eps.bmi.search.parsing.TextParser;
+import java.util.List;
+
+/**
+ *
+ * @author chus
+ */
+public class StopwordIndex implements Index{
+
+    @Override
+    public void build(String inputCollectionPath, String outputIndexPath, TextParser textParser) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void load(String indexPath) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<String> getDocumentIds() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public TextDocument getDocument(String documentId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<String> getTerms() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Posting> getTermPostings(String term) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
