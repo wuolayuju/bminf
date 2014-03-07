e408d7ab200d36ee7ac204898ef299ea37258ec9
Ari <ari.hangam@gmail.com>
2014-03-07 18:16:11 +0100

    Cosicas de SearcherTest

--- Practica3/src/es/uam/eps/bmi/search/searching/SearcherTest.java
+++ Practica3/src/es/uam/eps/bmi/search/searching/SearcherTest.java
@@ -9,11 +9,18 @@ package es.uam.eps.bmi.search.searching;
 import es.uam.eps.bmi.search.ScoredTextDocument;
 import es.uam.eps.bmi.search.indexing.AdvancedIndex;
 import es.uam.eps.bmi.search.indexing.BasicIndex;
+import es.uam.eps.bmi.search.indexing.Index;
 import es.uam.eps.bmi.search.indexing.StemIndex;
 import es.uam.eps.bmi.search.indexing.StopwordIndex;
 import es.uam.eps.bmi.search.parsing.HTMLStemParser;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -61,7 +68,7 @@ public class SearcherTest {
         testCollection(path_100k);
     }
 
-    private static void testCollection(String collectionPath) {
+    private static void testCollection(String collectionPath) throws Exception {
         
         List<String> indexesPaths = new ArrayList<>();
         
@@ -87,21 +94,71 @@ public class SearcherTest {
         
         // Fichero de consultas y relevancias
         String queriesPath = collectionPath + "queries.txt";
-        collectionPath.lastIndexOf("-");
         
-        /**
-         * TODO : Detectar el tipo de coleccion para sacar el nombre de relevancia
-         */
-        
-        String relevancePath = collectionPath + "relevance.txt";
+        String collectionName = collectionPath.substring(
+                collectionPath.lastIndexOf("-"),
+                collectionPath.lastIndexOf("/"));
+        String relevancePath = collectionPath + "relevance-" + collectionName + ".txt";
         
         for (String indexPath : indexesPaths) {
             basicIndex.load(indexPath);
+            
+            testIndex(basicIndex, queriesPath, relevancePath);
             
             /**
             * TODO : Buscar en cada indice las consultas y construir P@5 y P@10
             */ 
         }
+    }
+
+    private static void testIndex(Index index, String queriesPath, String relevancePath) throws Exception {
+        // Lectura de las consultas y las relevancias
+        List<String> listQueries = readQueries(queriesPath);
+        List<List<String>> listsRelevance = readRelevance(relevancePath);
+        
+        BooleanSearcher booleanSearcher = new BooleanSearcher();
+        
+        List<Double> precisions = calcPrecisions(booleanSearcher, listQueries, listsRelevance);
+        
+    }
+
+    private static List<Double> calcPrecisions(Searcher searcher, List<String> queries, List<List<String>> relevances) {
+        
+        int i = 0;
+        for (String query : queries) {
+            List<ScoredTextDocument> listResults = searcher.search(query);
+            List<String> listRelevance = relevances.get(i);
+        }
+        
+        
+        return null;
+    }
+    
+    private static List<String> readQueries(String queriesPath) throws Exception {
+        BufferedReader inQueries = new BufferedReader(
+                     new InputStreamReader(new FileInputStream(queriesPath), "UTF-8"));
+        
+        List<String> queries = new ArrayList<>();
+        String line;
+        while ((line = inQueries.readLine()) != null) {
+            queries.add(line.substring(line.indexOf(":")+1));
+        }
+        
+        return queries;
+    }
+
+    private static List<List<String>> readRelevance(String relevancePath) throws Exception {
+        BufferedReader inRelevance = new BufferedReader(
+                     new InputStreamReader(new FileInputStream(relevancePath), "UTF-8"));
+        
+        List<List<String>> listsRelevance = new ArrayList<>();
+        String line;
+        while ((line = inRelevance.readLine()) != null) {
+            List<String> relev = Arrays.asList(line.split("\t"));
+            listsRelevance.add(relev);
+        }
+        
+        return listsRelevance;
     }
     
 }
