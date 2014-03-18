ebe1d61b2e6b845c39f75e40c82a7fbe351b2495
Ari <ari.hangam@gmail.com>
2014-03-18 12:48:58 +0100

    Creados los esqueletos para PageRank y sus respectivas pruebas.

---
+++ Practica4/src/es/uam/eps/bmi/search/ranking/graph/PageRank.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.uam.eps.bmi.search.ranking.graph;
+
+/**
+ *
+ * @author uam
+ */
+public class PageRank {
+    
+    public double getScoreOf(String documentId) {
+        return 0.0;
+    }
+}
