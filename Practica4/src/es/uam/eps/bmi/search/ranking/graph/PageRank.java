5d6536b6ed8ff87d0f09e1aff0187535f4aeaf90
deisi <deisi@192.168.1.8>
2014-03-19 14:55:04 +0100

    Empazado PageRank.

--- Practica4/src/es/uam/eps/bmi/search/ranking/graph/PageRank.java
+++ Practica4/src/es/uam/eps/bmi/search/ranking/graph/PageRank.java
@@ -8,14 +8,17 @@ package es.uam.eps.bmi.search.ranking.graph;
 
 import edu.uci.ics.jung.graph.*;
 import edu.uci.ics.jung.graph.util.EdgeType;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -32,13 +35,16 @@ public class PageRank {
     
          graph = new SparseGraph();
          nodes = new HashMap<>();
-        //graph = new DirectedSparseGraph<NodeGraph, Integer>();
+    }
+    
+    public void loadFromfile (String fileGraph) throws FileNotFoundException, IOException {
         
         BufferedReader br = new BufferedReader(
                 new InputStreamReader(
                         new DataInputStream(
                                 new FileInputStream(fileGraph))));
 
+        int numNodes = countLinesFile(fileGraph);
         String line;
         int edgeCnt = 0;
         while ((line = br.readLine()) != null) {
@@ -51,7 +57,7 @@ public class PageRank {
             int outlinks = Integer.parseInt(st.nextToken());
             List<Object> outrank = new ArrayList<>();
             outrank.add(outlinks);
-            outrank.add(1);
+            outrank.add(1 / numNodes);
             if (nodes.containsKey(source)) { nodes.remove(source); }
             nodes.put(source, outrank);
             
@@ -64,11 +70,40 @@ public class PageRank {
         br.close();
     }
     
+    private int countLinesFile(String filename) throws IOException {
+        InputStream is = new BufferedInputStream(new FileInputStream(filename));
+        try {
+            byte[] c = new byte[1024];
+            int count = 0;
+            int readChars = 0;
+            boolean empty = true;
+            while ((readChars = is.read(c)) != -1) {
+                empty = false;
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n') {
+                        ++count;
+                    }
+                }
+            }
+            return (count == 0 && !empty) ? 1 : count;
+        } finally {
+            is.close();
+        }
+    }
+    
     public double getScoreOf(String documentId) {
         return 0.0;
     }
     
-    public void calculatePageRank() {
+    public void calculatePageRank(double tolerance, double damping) {
         
+        do {
+            Iterator<String> itr = graph.getVertices().iterator();
+            while(itr.hasNext()) {
+                String id = itr.next();
+                double pr = damping / graph.getVertexCount();
+                //graph.getPredecessors(id);
+            }
+        } while(true);
     }
 }
