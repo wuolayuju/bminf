9ba81872a9dbb8384012ae7831df7a916c0c67b4
Ari <ari.hangam@gmail.com>
2014-03-27 12:08:41 +0100

    Añadido resultados de Proximal (FALTAN RESULTADOS CUALITATIVOS).

--- Practica4/src/es/uam/eps/bmi/search/searching/ProximalSearcher.java
+++ Practica4/src/es/uam/eps/bmi/search/searching/ProximalSearcher.java
@@ -12,7 +12,6 @@ import es.uam.eps.bmi.search.indexing.Posting;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.PriorityQueue;
 
